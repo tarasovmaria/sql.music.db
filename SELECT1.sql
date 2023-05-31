e1e7446c22d48c90072bf5003bee2daed9b07b19
Mariia Tarasova <115806295+tarasovmaria@users.noreply.github.com>
2023-05-31 09:50:42 +0400

    Update SELECT1.sql

--- SELECT1.sql
+++ SELECT1.sql
@@ -1,7 +1,7 @@
 -- Название и год выхода альбомов, вышедших в 2018 году.
 
 SELECT title, release_date FROM albums
-WHERE release_date LIKE '2018%';
+WHERE CAST(a.release_date AS TEXT) LIKE '2018%';
 
 -- Название и продолжительность самого длительного трека.
 SELECT name, duration FROM tracks 
@@ -13,7 +13,7 @@ WHERE duration > 3.30;
 
 -- Названия сборников, вышедших в период с 2018 по 2020 год включительно.
 SELECT title FROM collections
-WHERE release_date like '2018%' OR release_date LIKE '2019%' OR release_date LIKE '2020%';
+WHERE CAST(a.release_date AS TEXT) like '2018%' OR CAST(a.release_date AS TEXT) LIKE '2019%' OR CAST(a.release_date AS TEXT) LIKE '2020%';
 
 -- Исполнители, чьё имя состоит из одного слова.
 SELECT alias FROM music_artists
@@ -21,4 +21,4 @@ WHERE (LENGTH(alias)-LENGTH(REPLACE(alias,' ',''))+1)=1;
 
 -- Название треков, которые содержат слово «мой» или «my».
 SELECT name FROM tracks
-WHERE name LIKE '%my%' OR '%мой%';
+WHERE name LIKE '%my%' OR name LIKE '%мой%';
